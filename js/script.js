76d20d4abf291fddc273ee0ea1fed5e18e2624cb
freeCodeSpace <temp4email02@gmail.com>
2015-08-11 16:03:20 +0400

    add1

--- js/script.js
+++ js/script.js
@@ -6,10 +6,4 @@ $(function() {
       'wrapAround': true // вкл. круговой просмотр
     });
 
-    // Настройки accordion
-    $( ".accordion" ).accordion({
-        collapsible: true, // позволяет сворачиваться
-        active: 1, // 1-й заголовок будет свернутый
-    });
-
 });
